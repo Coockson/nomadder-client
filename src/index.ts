7393d3dd1b72fba5d93abc522eac1611b9d3de55
khaancc <khaancc@gmail.com>
2019-11-08 02:16:31 +0100

    basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { INomadderEvent } from "./models/nomadder-event.model";
-
-
 //Imports
+import { INomadderEvent, EventTypes } from "./models/nomadder-event.model";
+import { IServerData } from "./models/server-data.model";
+
+
+//Global vars
+let wsc: WebSocket;
+const nomadder_key: string= "NOMADDER-DATA";
 
 export function initializeSocket(serverURL?: string){
     if (serverURL){
@@ -12,29 +16,27 @@ export function initializeSocket(serverURL?: string){
     else{
         var ws = new WebSocket("ws://localhost:8080");
         return ws
-    }
-    
+    }    
 }
 
+export function getLocalData(){ 
+    var data= localStorage.getItem(nomadder_key);
+    return data
+}
 
-export function startNomadder(){
-    var wsc: WebSocket = initializeSocket(); //localhost for now
+export function connectToServer(){
 
-    //@ts-ignore
-    var allData = Object.entries(localStorage);
-    var allKeys = Object.keys(localStorage);
+    wsc = initializeSocket(); //localhost for now
+  
+    var data = getLocalData();
+    if (data === null){
+        console.log("No data initiated on client");
+    } else{
+        wsc.onopen = () => wsc.send(<string>data);
+    }  
+}  
 
-    //var data = localStorage.getItem(nomadder_key);
-    //var json_data = JSON.stringify(data);
-
-    if(allKeys){
-        wsc.onopen = () => {
-            for(var i=0; i<allKeys.length ; i++ ){
-                wsc.send(allData[i][1]);
-            }           
-        }
-    }
-
+export function listenBatch(){
     // Wait for batch protocol
     wsc.addEventListener("message", ( message) => {
         var msg: INomadderEvent = message as unknown as INomadderEvent; 
@@ -46,15 +48,27 @@ export function startNomadder(){
         if (!msg.protocolInformation.event) {
             return;
         }
-        switch (msg.protocolInformation.event) { //will add models later
-            case "BATCH":
-                // the data is good to save
-                localStorage.setItem(msg.hash, JSON.stringify(msg));
-            default:
-                /*tslint:disable-next-line:no-console*/
-                console.error('[Unknown event type]: ', msg.protocolInformation.event);
-                break;
+        if (msg.protocolInformation.event == EventTypes.SYNC) {
+            saveBatchData(msg.protocolInformation.payload.data)
         }
-    });
+    }); 
+}
 
+export function saveBatchData(data: IServerData){
+    var local_storage = getLocalData();
+    var local_json = JSON.parse(<string>local_storage); //for now assuming there is data
+    //var serverID = data.serverId; required when multi server is added
+    var local_data = local_json.protocolInformation.payload.data;
+    //console.log(local_data);
+    local_data.data.push(data.data);
+    localStorage.removeItem(nomadder_key);                          // delete old data  
+    localStorage.setItem(nomadder_key, JSON.stringify(local_data)); // add updated data
+}
+
+export function JSONToSYNCPackage(data: JSON){
+    
+}
+
+export function JSONStringToSYNCPackage(data: string){
+    
 }
